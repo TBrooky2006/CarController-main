218788cf104939a4e46237c137303f4eaba7ea00
TBrooky2006 <BRO23004799@heartofyorkshire.ac.uk>
2025-01-14 12:53:43 +0000

    start

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float timer;
+
+    public float timeLimit;
+
+    public TMP_Text timerText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = timeLimit;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+      GameTimer();  
+    }
+
+    void GameTimer()
+    {
+        timerText.text = timer.ToString("F2");
+
+        if (timer <= 0)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+        else
+        {
+            timer -= Time.deltaTime;
+        }
+    }
+}
