0e090019b4ec01c1b1dd44332c4817d986d5a98c
TBrooky2006 <BRO23004799@heartofyorkshire.ac.uk>
2025-02-10 16:01:38 +0000

    s

---
+++ Assets/Scripts/Platform moving.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platformmoving : MonoBehaviour
+{
+    public bool canMove;
+
+    [SerializeField] float speed;
+    [SerializeField] int startPoint;
+    [SerializeField] Transform[] points;
+
+    int i;
+    bool reverse;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = points[startPoint].position;
+        i = startPoint;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(Vector3.Distance(transform.position, points[i].position) < 0.01f)
+        {
+            canMove = true;
+
+            if(i== points.Length - 1)
+            {
+                reverse = true;
+                i--;
+                return;
+            }   else if(i == 0) 
+            {
+                reverse = true;
+                i++;
+                return;
+            }
+
+            if(reverse)
+            {
+                i--;
+            }
+            else
+            {
+               i = (i + 1) ; 
+            }
+        }
+
+      if(canMove)
+      {
+        transform.position = Vector3.MoveTowards(transform.position, points[i].position, speed * Time.deltaTime);
+      }  
+    }
+}
